afc50dc853cd710c29413ef47ac60f830fe5e9f8
molrobot <john19990708@gmail.com>
2020-07-19 15:47:03 +0800

    add config file

--- crawler.py
+++ crawler.py
@@ -5,83 +5,97 @@ import logging
 import datetime
 import requests
 import smtplib
+import json
+import configparser
 from email.mime.text import MIMEText
 from email.header import Header
 from bs4 import BeautifulSoup
 
-url = "https://www.costco.com.tw/Health-Beauty/Home-Health-Care/Hygiene-Masks-First-Aid/CSD-Medical-Color-Mask-50-Pieces/p/224368"
-title = "中衛醫療彩色口罩"
-result = False
-sent = False
-nowtime = None
+class costco:
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s, %(levelname)s: %(message)s", \
+                            datefmt="%Y-%m-%d %H:%M:%S")
 
-def send_email():
-    global nowtime, sent
-    smtp=smtplib.SMTP("smtp.gmail.com", 587)
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login("sender@gmail.com", "password")
-    
-    from_addr = "sender@gmail.com"
-    to_addr = "receiver@gmail.com"
+        config = configparser.RawConfigParser()
+        config.read("crawler.config", encoding="utf-8")
 
-    s = nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + url
-    
-    message = MIMEText(s, "plain", "utf-8")
-    message["From"] = Header("Python", "utf-8")
-    message["To"] = Header(to_addr, "utf-8")
-    message["Subject"] = Header("好市多中衛口罩上架", "utf-8")
-    
-    try:
-        status = smtp.sendmail(from_addr, to_addr, message.as_string())
-        if status == {}:
-            print("傳送成功!")
-            sent = True
-        else:
-            print("傳送失敗!")
-    except smtplib.SMTPException:
-        print("傳送失敗!")
+        # 設定目標商品
+        self.url = config.get("product", "url")
+        self.title = config.get("product", "title")
+        # 設定信箱
+        self.server = config.get("email", "server")
+        self.port = config.getint("email", "port")
+        self.user = config.get("email", "user")
+        self.password = config.get("email", "password")
+        self.from_addr = config.get("email", "from_addr")
+        self.to_addr = config.get("email", "to_addr")
 
-    smtp.quit()
+        # 設定等待時間
+        self.next_send_time = config.getint("time", "next_send_time")
+        self.next_search_time = config.getint("time", "next_search_time")
 
-def search():
-    try:
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, "lxml")
-        # title in res.text
-        if (soup.find(id="add-to-cart-button-224368") != None or soup.find(id="addToCartButton") != None):
+        # 設定 user-agent
+        self.USER_AGENT_LIST = config.get("agent", "user_agent")
+
+        # 開始執行
+        self.start()
+
+    def start(self):
+        while True:
+            self.nowtime = datetime.datetime.now(pytz.timezone("Asia/Taipei"))
+            if self.checktime():
+                result = self.search()
+                if result:
+                    logging.info(self.url)
+                    if self.send_email(self.nowtime):
+                        time.sleep(600)
+                else:
+                    logging.info(result)
+            time.sleep(random.random() * 30 + 30)
+
+    def search(self):
+        try:
+            header = {
+                "user-agent": random.choice(self.USER_AGENT_LIST)
+            }
+            with requests.get(self.url, headers=header) as res:
+                soup = BeautifulSoup(res.text, "lxml")
+                if (soup.find(id="add-to-cart-button-224368") != None or soup.find(id="addToCartButton") != None):
+                    return True
+        except OSError as e:
+            print(e)
+
+        return False
+
+    def checktime(self):
+        if 7 <= self.nowtime.hour <= 22 and self.nowtime.minute % 30 < 30:
             return True
-    except OSError as e:
-        print(e)
+        return False
 
-    return False
+    def send_email(self, nowtime):
+        text = nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + self.url
 
-def checktime():
-    global nowtime
-    if 7 <= nowtime.hour <= 22 and nowtime.minute % 30 < 10:
-        return True
-    return False
+        message = MIMEText(text, "plain", "utf-8")
+        message["From"] = Header("Python Crawler", "utf-8")
+        message["To"] = Header(self.to_addr, "utf-8")
+        message["Subject"] = Header("好市多中衛口罩上架", "utf-8")
+
+        with smtplib.SMTP(self.server, self.port) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.login(self.user, self.password)
+            status = smtp.sendmail(self.from_addr, self.to_addr, message.as_string())
+            smtp.quit()
+            if status == {}:
+                print("傳送成功!")
+                return True
+
+        print("傳送失敗!")
+        return False
 
 def main():
-    global nowtime, result, sent
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s, %(levelname)s: %(message)s",
-                        datefmt="%Y-%m-%d %H:%M:%S")
-    while True:
-        nowtime = datetime.datetime.now(pytz.timezone("Asia/Taipei"))
-        if checktime():
-            result = search()
-            if result:
-                logging.info(url)
-                send_email()
-            else:
-                logging.info(result)
+    csd = costco()
 
-            if sent:
-                time.sleep(1000)
-                result = False
-                sent = False
-
-        time.sleep(random.random() * 60)
 
 if __name__ == "__main__":
     main()
