1661fb6fd853363b45c8161f903d46bd9b5151d6
molrobot <john19990708@gmail.com>
2020-08-20 14:51:34 +0800

    可指定爬取多樣商品

--- crawler.py
+++ crawler.py
@@ -28,12 +28,14 @@ class costco:
             print("could not find", configFileName)
             exit()
 
+        productFileName = "product.json"
+        if not os.path.isfile(productFileName):
+            print("could not find", productFileName)
+            exit()
+
+        # 讀取設定
         with open(configFileName, "r", encoding="utf-8") as json_file:
             config = json.load(json_file)
-
-            # 設定目標商品
-            self.url = config["product"]["url"]
-            self.title = config["product"]["title"]
 
             # 設定 Line API
             self.line_bot_token = config["line"]["line_bot_channel_access_token"]
@@ -53,12 +55,20 @@ class costco:
             # 設定 user-agent
             self.USER_AGENT_LIST = config["agent"]["user-agent"]
 
-        # 檢查網址為商品頁面或分類列表
-        # 切割網址取得商品編號
-        if self.url.rsplit("/", 2)[1] == "p":
-            self.id = self.url.rsplit("/", 1)[1]
-        else:
-            self.id = None
+        # 設定目標商品
+        self.product = list()
+        with open(productFileName, "r", encoding="utf-8") as json_file:
+            config = json.load(json_file)
+
+            for item in config:
+                # 檢查網址為商品頁面或分類列表
+                # 切割網址取得商品編號
+                if item["url"].rsplit("/", 2)[1] == "p":
+                    item["id"] = item["url"].rsplit("/", 1)[1]
+                else:
+                    item["id"] = None
+                item["status"] = 0
+                self.product.append(item)
 
         self.line_bot_api = LineBotApi(self.line_bot_token)
 
@@ -74,27 +84,28 @@ class costco:
         1 :有上架但無庫存(分類清單中有出現或商品網頁存在，但加入購物車按鈕不存在)
         2 :有上架且"可能"有庫存(分類清單中有出現或商品網頁存在，且加入購物車按鈕存在)
         '''
-        self.product_status = 0
+
 
     def start(self):
         while True:
             self.nowtime = datetime.now(pytz.timezone("Asia/Taipei"))
             if self.check_time():
-                result = self.search()
-                if result:
-                    logging.info(self.url)
-                    if self.send_email():
-                        time.sleep(self.next_send_time)
-                else:
-                    logging.info(result)
-            time.sleep(random.randint(5, self.next_search_time))
+                for item in self.product:
+                    result = self.search(item)
+                    if result != item["status"]:
+                        item["status"] = result
+                        logging.info(item["title"] + " " + item["url"])
+                        self.send_line(item)
+                        self.send_email(item)
+            time.sleep(random.randint(10, self.next_search_time))
+
 
     # 爬取資料，檢查按鈕是否存在
-    def search(self):
+    def search(self, item):
         header = {
             "user-agent": random.choice(self.USER_AGENT_LIST)
         }
-        with requests.get(self.url, headers=header) as res:
+        with requests.get(item["url"], headers=header) as res:
             soup = BeautifulSoup(res.text, "lxml")
             '''
             商品頁面存在，可以找到 addToCartButton
@@ -102,17 +113,21 @@ class costco:
             出現"加入購物車"按鈕不代表一定有庫存
             若不知道商品網址或編號，只有商品分類網址跟商品名稱，就直接搜尋名稱，但無法檢查按鈕
             '''
-            if self.id != None:
-                return (soup.find(id=("add-to-cart-button-" + self.id)) != None or soup.find(id="addToCartButton") != None)
-            else:
-                return (self.title in res.text)
-        return False
+            if item["id"] != None and (soup.find(id="addToCartButton") != None or \
+                                    soup.find(id=("add-to-cart-button-" + item["id"])) != None):
+                return 2
+            elif item["title"] in res.text:
+                return 1
+        return 0
 
+
+    # 自動加入購物車
     def add_to_cart(self):
         pass
 
     def checkout(self):
         pass
+
 
     # 自訂時間範圍檢查
     def check_time(self):
@@ -122,14 +137,15 @@ class costco:
             return True
         return False
 
+
     # 寄信通知
-    def send_email(self):
-        text = self.nowtime.strftime("%Y-%m-%d %H:%M:%S ") + self.title + "\n" + self.url
+    def send_email(self, item):
+        text = self.nowtime.strftime("%Y-%m-%d %H:%M:%S ") + item["title"] + "\n" + item["url"]
 
         msg = MIMEText(text, "plain", "utf-8")
         msg["From"] = Header("好市多爬蟲", "utf-8")
         msg["To"] = Header(self.to_addr, "utf-8")
-        msg["Subject"] = Header(self.message[self.product_status], "utf-8")
+        msg["Subject"] = Header(self.message[item["status"]], "utf-8")
 
         with smtplib.SMTP(self.server, self.port) as smtp:
             smtp.ehlo()
@@ -142,11 +158,11 @@ class costco:
         logging.info("郵件傳送失敗")
         return False
 
+
     # 傳line通知
-    def send_line(self):
-        text = self.message[self.product_status] + "\n" + \
-               self.nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + self.title + "\n" + self.url
-        
+    def send_line(self, item):
+        text = self.message[item["status"]] + "\n" + \
+               self.nowtime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + item["title"] + "\n" + item["url"]
         try:
             self.line_bot_api.broadcast(TextSendMessage(text=text))
         except LineBotApiError as e:
